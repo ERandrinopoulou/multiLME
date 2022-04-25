da9ee1907c2768faaa92ef4b962d3730a9a4ca46
Eleni-Rosalina Andrinopoulou <e.andrinopoulou@erasmusmc.nl>
2022-04-25 13:32:28 +0200

    add uncorrelated RE

--- R/mv_lme function.R
+++ R/mv_lme function.R
@@ -48,7 +48,7 @@ mv_lme <- function(formulas, data, families, hc,
 
   # Add matrix inv.D for uncorrelated random effects.
   if (corr_RE == FALSE) {
-    Data1$inv.D <- matrix(0, nb, nb)
+    Data1$inv.D <- matrix(0, n_RE, n_RE)
     diag(Data1$inv.D) <- NA
   }
 
