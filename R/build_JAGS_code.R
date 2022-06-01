7aa94631b4df94b5893b3edc84e2e8fc546c5298
ERASMUSMC\015004 <e.andrinopoulou@erasmusmc.nl>
2022-06-01 17:08:35 +0200

    normalized area

--- R/build_JAGS_code.R
+++ R/build_JAGS_code.R
@@ -46,8 +46,8 @@ JAGSmodel <- function(families, hc, predicted, corr_RE,
 
             if (norm_area == TRUE){
 
-              ind_time_var <- c(1:dim(Data1$X1)[2])[colnames(Data1$X1) == "year"]
-              mu_alpha <- paste0(" * (f_derivY", assoc_from, "[j]/(X", assoc_from, "[j,", ind_time_var, "]+0.01))")
+              ind_time_var <- c(1:dim(Data1$X1)[2])[colnames(Data1$X1) == time_var]
+              mu_alpha <- paste0(" * (f_derivY", assoc_from, "[j]/(X", assoc_to, "[j,", ind_time_var, "]+0.01))")
             } else {
               mu_alpha <- paste0(" * f_derivY", assoc_from, "[j]")
             }
@@ -118,8 +118,8 @@ JAGSmodel <- function(families, hc, predicted, corr_RE,
           if (!is.null(extraForm)){
 
             if (norm_area == TRUE){
-              ind_time_var <- c(1:dim(X1)[2])[colnames(Data1$X1) == "year"]
-              mu_alpha <- paste0(" * (f_derivY", assoc_from, "[j]/(X", assoc_from, "[j,", ind_time_var, "]+0.01))")
+              ind_time_var <- c(1:dim(X1)[2])[colnames(Data1$X1) == time_var]
+              mu_alpha <- paste0(" * (f_derivY", assoc_from, "[j]/(X", assoc_to, "[j,", ind_time_var, "]+0.01))")
             } else {
               mu_alpha <- paste0(" * f_derivY", assoc_from, "[j]")
             }
